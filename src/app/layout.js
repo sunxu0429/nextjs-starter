a6d50bb490fce0b328a8e6e0cee118480ddb516d
sun.xu <@Sea23se>
2024-03-03 15:46:23 +0800

    init project

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Next App',
+  description: 'Next.js starter app',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <h1>This is the main layout.</h1>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
