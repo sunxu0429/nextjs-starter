17c8db2405c7b6bce656bb06e7272da9502b0c5a
sun.xu <@Sea23se>
2024-03-04 19:03:19 +0800

    add media query for menu

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,7 @@
+"use client";
 import NavLink from "./navLink/navLink";
 import styles from "./links.module.css";
+import { useState } from "react";
 const Links = () => {
   const links = [
     {
@@ -22,21 +24,37 @@ const Links = () => {
 
   const session = true;
   const isAdmin = true;
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && (
-            <NavLink item={{ title: "Admin", path: "/admin" }}></NavLink>
-          )}
-          <button className={styles.logout}>Log Out</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && (
+              <NavLink item={{ title: "Admin", path: "/admin" }}></NavLink>
+            )}
+            <button className={styles.logout}>Log Out</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title}></NavLink>
+          ))}
+        </div>
       )}
     </div>
   );
