dca87800ec4c7c7c4b4fea21185d9b46d7bbe5d8
sun.xu <@Sea23se>
2024-03-12 19:41:04 +0800

    update home page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
   return (
-    <div>About Page</div>
-  )
-}
-export default AboutPage
+    <div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="https://images.pexels.com/photos/19457037/pexels-photo-19457037/free-photo-of-street-by-palais-garnier-in-paris-france.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          fill
+        />
+        ;
+      </div>
+    </div>
+  );
+};
+export default AboutPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      Home Page
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}> Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem,ipsum dolor sit amet consectetur adipisc elit.Vero blanditiis
+          adiposc minina reicasead a autuem assumead dolire.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImage} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImage} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
